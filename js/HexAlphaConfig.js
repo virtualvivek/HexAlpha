a0e159c3fda64df1cd4d289754fb5c1464d7c569
Vivek Verma <vivekverma2499@gmail.com>
2020-02-13 11:22:01 +0530

    Added js files -v1.0

---
+++ js/HexAlphaConfig.js
@@ -0,0 +1,9 @@
+HexAlphaTo('--App_Color','0.5','--Alpha_Color');
+
+HexAlphaTo('#018574','0.7','--Alpha_Color2');
+
+let alphaColor4 = HexAlpha('--App_Color4','0.4');
+document.getElementById("Img4Alpha").style.background=alphaColor4;
+
+let alphaColor5 = HexAlpha('#C30052','0.5');
+document.getElementById("Img5Alpha").style.background=alphaColor5;
